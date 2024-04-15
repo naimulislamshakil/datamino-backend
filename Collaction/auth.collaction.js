43bdc7a5910e5f5d5f04c29e6a7a50c5cf766b37
Naimul Islam <ms0413354@gmail.com>
2024-04-15 09:09:44 +0600

    complete login process

--- Collaction/auth.collaction.js
+++ Collaction/auth.collaction.js
@@ -1,5 +1,7 @@
 const { USER_NOT_FOUND } = require('../constants/errorCode');
 const { singupService, isUserExist } = require('../services/auth.service');
+const bcrypt = require('bcrypt');
+const { generateToken } = require('../utils/token');
 
 exports.registationCollaction = async (req, res, next) => {
 	try {
@@ -19,6 +21,42 @@ exports.registationCollaction = async (req, res, next) => {
 			success: true,
 			status: 200,
 			message: 'Usae Create Successfully...',
+		});
+	} catch (error) {
+		return next(error);
+	}
+};
+
+exports.loginCollaction = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await isUserExist(email);
+		if (!user) {
+			const error = {
+				statusCode: USER_NOT_FOUND,
+				message: 'User Not Exist....',
+			};
+			return next(error);
+		}
+
+		const match = bcrypt.compareSync(password, user.password);
+
+		if (!match) {
+			const error = {
+				statusCode: USER_NOT_FOUND,
+				message: 'Invalid credentials.',
+			};
+			return next(error);
+		}
+
+		const token = generateToken(email, user._id);
+
+		res.status(200).json({
+			success: true,
+			status: 200,
+			message: 'User Successfully Login..',
+			token,
 		});
 	} catch (error) {
 		return next(error);
--- Router/v1/auth.route.js
+++ Router/v1/auth.route.js
@@ -1,8 +1,11 @@
-const { registationCollaction } = require('../../Collaction/auth.collaction');
+const {
+	registationCollaction,
+	loginCollaction,
+} = require('../../Collaction/auth.collaction');
 const { userRegistationValidation } = require('../../utils/userValidation');
 
 const route = require('express').Router();
 
 route.post('/singup', userRegistationValidation, registationCollaction);
-
+route.post('/login', loginCollaction);
 module.exports = route;
